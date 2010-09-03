edae03926922410cf114ef1c5b42f5cd94abf678
Malte Reißig <Malte Reiig>
2010-09-03 12:57:24 +0200

    creating a foldertopic, creating a canvas and a foldercanvas relation works
    what is still missing:
    a) each item should get an incoming: META_HAS_INSTANCE relation to it's typeUri
    b) proper json encoding - introduced a json hack utility for extracting numbers from json strings
    c) correct filePath serialization as property "value" when empty spaces are contained

--- deepamehta3-nautilus.py
+++ deepamehta3-nautilus.py
@@ -3,7 +3,8 @@ import syslog
 import nautilus
 import webbrowser
 import urllib
-import simplejson as json
+import simplejson
+import sys
 
 #
 from restclient import GET, POST, PUT, DELETE
@@ -73,14 +74,19 @@ class DeepMenuProvider(nautilus.MenuProvider):
         
     def menu_activate_view(self, menu, file):
         #
-        filename = urllib.unquote(file.get_uri()[7:])
-        existent = self.askForFolderCanvas(filename)
-        if existent:
-            self.updateFolderCanvasTopic(filename)
+        filePath = urllib.unquote(file.get_uri())
+        fileName = urllib.unquote(file.get_uri()[7:])
+        folderId = self.askForFolderCanvas(filePath)
+        if folderId != '':
+            syslog.syslog("monty.viewWithDeepaMehta:folderId => " + folderId + " is already known")
+            self.updateFolderCanvasTopic(fileName)
         else:
-            # self.createFolderCanvasTopic(filename)
-            # will not create foldercanvas topics now - would create empty topicmaps all the time
-            syslog.syslog(' -- is not creating topicmaps -- ');
+            topicId = self.createFolderTopic(file.get_uri())
+            syslog.syslog("monty.creatingCanvasTopic now for title: " + fileName + " and " + topicId)
+            relationId = self.createCanvasTopic(fileName, topicId)
+            syslog.syslog("monty.createdFolderCanvasRelation.resultId " + repr(relationId))
+            # relationId = self.createRelation('FOLDER_CANVAS', topicId, canvasId)
+            # syslog.syslog(' --  to create foldercanvas from ' + topicId + 'to -- ' + canvasId + ' => ' + relationId)
         files = []
         folders = []
         listedItems = os.listdir(filename)
@@ -136,26 +142,128 @@ class DeepMenuProvider(nautilus.MenuProvider):
     def askForFolderCanvas(self, folderPath):
         # http://localhost:8080/core/topic/by_property/{key}/{value}
         key = 'de/deepamehta/core/property/Path'.replace("/", "%2F")
-        key = key.replace(" ", "%20")
+        # key = key.replace(" ", "%20")
         #
         value = folderPath.replace("/", "%2F")
-        value = value.replace(" ", "%20")
+        #value = value.replace(" ", "%20")
         url = SERVICE_URL + '/topic/by_property/' + key + '/' + value
-        # syslog.syslog("monty.dmc.askForFolderCanvas:" + url)
+        syslog.syslog("monty.askForFolderCanvas: " + folderPath)
         response = GET(url,
-            headers={'Content-Type' : 'application/json'}, 
+            headers={'Content-Type' : 'application/json', 'TE' : 'UTF-8'}, 
             accept=['application/json', 'text/plain']
         )
-        # syslog.syslog(' ---: ' + response + ' :--- ')
-        if response == '':
+        resultId = self.getFieldFromResponse(response, 'id')
+        if resultId == '':
             syslog.syslog(' ---: That folder is yet unknown to your DeepaMehta installation :--- ')
-            return False
-        else: 
-            syslog.syslog(' ---: folderId is:' + response + ' :--- ')
-            # response.indexOf("\"id\":") + 5, 3 -- strip Id Field
-            # take that ID and check for a Topicmap-Topic which is related to that via a FOLDER_CANVAS-Relation
-            return True
+            return resultId
+        else:
+            try:
+                correctedString = unicode("'" + str(response) + "'", "UTF-8")
+                # uncomment the following line ...
+                # syslog.syslog(" ---: monty.askForFolderCanvas RESPONSE: " + correctedString + " :--- ")
+                # and paste the result of the line of this from your console as it is into your python interpreter..
+                # in the following way, the result is astonishing cause decoding json works that way
+                # result = simplejson.loads(correctedString)
+                # the result is fine in the interpreter there !?
+                # resultId = result['id'] 
+                # now trying to get an exception with decoding the string to a proper format
+                decodedString = correctedString.decode("UTF-8")
+            except Exception, e:
+                syslog.syslog("DecodingException: \"" + str(e) + "\" for JSON of ---: " + correctedString)
+            syslog.syslog("monty.askForFolder.folderId => " + resultId + " :--- ")
+            return resultId
     
+    
+    
+    # typeUri=de/deepamehta/core/topictype/TopicmapRelationRef
+    # of type "RELATION" from topicOne->topicTwo
+    # of type "RELATION" from topicmapId->resultingRelationId
+    # create a relation of type_uri between two topicIds
+    def createRelation(self, relation_type_uri, topicOne, topicTwo):
+        message = '{"type_id":"' + relation_type_uri + '","src_topic_id":'+topicOne+',"dst_topic_id":'+topicTwo+',"properties":{}}'
+        # syslog.syslog('monty.createRelation.sending ---: ' + repr(message))
+        try:
+            response = POST(SERVICE_URL + '/relation/',
+              headers={'Content-Type' : 'application/json',  'Cookie' : 'workspace_id=62'}, 
+              accept=['application/json', 'text/plain'],
+              body = message
+            )
+            # response: {"id":541,"type_id":"RELATION","src_topic_id":98,"dst_topic_id":100,"properties":{}}
+            resultId = self.getFieldFromResponse(response, 'id')
+            syslog.syslog('monty.createRelation.resultId => ' + resultId)
+            return resultId
+        except BaseException, e:
+            syslog.syslog('EXCEPTION: ' + e)
+        return ''
+    
+    # create a new topicmap
+    def createNewTopicmap(self, title):
+        # to /topic = 
+        syslog.syslog("monty.createNewTopicmap.title => \"" + title + "\"")
+        postBody = '{"type_uri":"de/deepamehta/core/topictype/Topicmap","properties":{"de/deepamehta/core/property/Title":"' + title + '"}}'
+        try:
+            response = POST(SERVICE_URL + '/topic',
+                headers={'Content-Type' : 'application/json', 'Cookie' : 'workspace_id=62' },
+                accept=['application/json', 'text/plain', 'text/html'],
+                body = postBody
+            )
+            # syslog.syslog("monty.createNewTopicmap.response => " + repr(response))
+            resultId = self.getFieldFromResponse(response, 'id')
+            syslog.syslog('monty.createNewTopicmap.resultId => ' + repr(resultId))
+            return resultId
+        except BaseException, e:
+            syslog.syslog('EXCEPTION: ' + e)
+        return ''
+    
+    #
+    # creates a topic of type de/deepamehta/core/topictype/Folder for the given pathName 
+    # NOTE: before executing this, check if one is already one there !
+    #
+    # FIXME: empty spaces in filePath are not encoded correctly
+    def createFolderTopic(self, filePath):
+        # syslog.syslog('monty.createFolderTopic: ' + filePath)
+        postBody = '{"type_uri":"de/deepamehta/core/topictype/Folder", "properties":{"de/deepamehta/core/property/Path":"' + filePath + '", "de/deepamehta/core/property/FolderName": "' + filePath[7:] + '"}}'
+        response = POST(SERVICE_URL + '/topic', 
+            headers={'Content-Type' : 'application/json', 'Cookie' : 'workspace_id=62' }, 
+            accept=['application/json', 'text/plain', 'text/html'], 
+            body = postBody)
+        try:
+            # syslog.syslog(' ---: ' + simplejson.loads(response)['id'] + ' :---')
+            resultId = self.getFieldFromResponse(response, 'id')
+            syslog.syslog('monty.createFolderTopic.resultId => ' + resultId)
+            return resultId
+        except SyntaxException:
+            syslog.syslog("SyntaxException while accessing the new folderTopicId")
+        return ''
+    
+    def createCanvasTopic(self, filePath, folderTopicId):
+        canvasIndex = str.rfind(filePath, "/")
+        canvasName = filePath[canvasIndex + 1:]
+        syslog.syslog('monty.createFolderCanvas: ' + canvasName + ' and folderId: ' + folderTopicId)
+        try:
+            #syslog.syslog(' ---: ' + json.dumps(prettyRe) + ' :---')
+            canvasId = self.createNewTopicmap(canvasName)
+            syslog.syslog("mondy.createFolderCanvasRelation from canvasId =>  " + canvasId + " to folderId => " + folderTopicId)
+            relationId = self.createRelation('FOLDER_CANVAS', folderTopicId, canvasId)
+            syslog.syslog('monty.createFolderCanvasRelation.resultId => ' + relationId + ' is now relating  ' + folderTopicId + ' to ' + canvasId)
+            return relationId
+        except Exception, e:
+            syslog.syslog('createAndRelateCanvasTopics.Exception: ' + e)
+        return ''
+    
+    def updateFolderCanvasTopic(self, filePath):
+        syslog.syslog('monty.dmc.updateFolderCanvas: ' + filePath + ' (not yet implemented)')
+    
+    def putTopicInMap(topicId):
+        # {"topic_id":81,"x":496.0975611412235,"y":107.05648882256608}
+        # not yet TODO: put in topicmap
+        # put: core/topicmap/id 
+        # body: {"relation_id":541}
+        # response: {"ref_id":177}
+        syslog.syslog('monty.dmc.putTopicInMap()')
+        
+    
+    # currently unused
     def getTopicsByType(self, topicTypeUri):
         # response = 
         # urllib.urlopen(SERVICE_URL + '/topic/by_type/' + topicTypeUri, None, ['Content-Type', 'application/json'])
@@ -164,7 +272,8 @@ class DeepMenuProvider(nautilus.MenuProvider):
             accept=['application/json', 'text/plain']
         )
         syslog.syslog(' ---: ' + response + ' :--- ')
-    
+        
+    # currently unused
     def getTopicById(self, topicId):
         syslog.syslog('monty.dmc.getTopicById: ' + topicId)
         response = GET(SERVICE_URL + '/topic/' + topicId, 
@@ -180,33 +289,25 @@ class DeepMenuProvider(nautilus.MenuProvider):
             accept=['application/json', 'text/plain', 'text/html'],
             body = '{ "type_uri":"de/deepamehta/core/topictype/File", "properties": { "de/deepamehta/core/property/FileName": "file:///home/malted/.nautilus/python-extensions/submenu.py", "de/deepamehta/core/property/Path":"", "de/deepamehta/core/property/MediaType":"text/plain", "de/deepamehta/core/property/Size":0, "de/deepamehta/core/property/Content":"Hello World!"}}'
         )
-        # ### ToDo: access id's or decode json
-        # idStart = response.find(':')
-        # idEnd = response.find('"', response.find(':')+1)
-        syslog.syslog(' ---: ' + response + ' :--- ')
-    
-    def createFolderCanvasTopic(self, filePath):
-        canvasIndex = str.rfind(filePath, "/")
-        canvasName = filePath[canvasIndex + 1:]
-        syslog.syslog('monty.dmc.createFolderCanvas: ' + canvasName)
-        postBody = '{"type_uri":"de/deepamehta/core/topictype/Topicmap","properties":{"de/deepamehta/core/property/Title":"' + canvasName + '"}}'
-        response = POST(SERVICE_URL + '/topic',
-            headers={'Content-Type' : 'application/json', 'Cookie' : 'workspace_id=62' },
-            accept=['application/json', 'text/plain', 'text/html'],
-            body = postBody
-        )
-        # prettyRe = json.JSONDecoder().decode(response)
-        prettyRe = json.loads(response)
-        try:
-            syslog.syslog(' ---: ' + json.dumps(prettyRe) + ' :---')
-            syslog.syslog(' ---: ' + prettyRe + ' :---')
-        except BaseException:
-            syslog.syslog('error while accessing python decoded json')
-    
-    def updateFolderCanvasTopic(self, filePath):
-        syslog.syslog('monty.dmc.updateFolderCanvas: ' + filePath)
-    
-    def putTopicInMap(topicId):
-        # {"topic_id":81,"x":496.0975611412235,"y":107.05648882256608}
-        syslog.syslog('monty.dmc.putTopicInMap()')
+
+
+
+    ###
+    ### --- Monty Parser Utility 
+    ###
+
+    # introduced cause of prblems wiht json decoding -- see line 162 and forthcoming for problemd description
+    # just tested with extracting numbers from JSON not working with string fields yet
+    def getFieldFromResponse(self, responseString, string):
+        field = '';
+        firstIndex = responseString.find('"' + string + '"')
+        #syslog.syslog('monty.getFieldFromResponse => ' + string +' firstIndex is: ' + repr(int(firstIndex)))
+        scndIndex = responseString.find('"', int(firstIndex) + 1)
+        #syslog.syslog('monty.getFieldFromResponse => ' + string +' scndIndex is: ' + repr(int(scndIndex)))
+        thirdIndex = responseString.find('"', int(scndIndex) + 1)
+        #syslog.syslog('monty.getFieldFromResponse => ' + string +' thirdIndex is: ' + repr(int(thirdIndex-2)))
+         # syslog.syslog('monty.getFieldFromResponse => ' + string +' from: ' + repr(int(scndIndex+2)) + ' to ' +  repr(thirdIndex-1))
+        field = responseString[int(scndIndex+2):int(thirdIndex-1)] # start, steps
+        # syslog.syslog('monty.getFieldFromResponse => ' + field)
+        return field
     
